47a51f10204f48acef16d298b6b32114b83257b3
Nahuel Sotelo <nsotelo@cristalmedia.net>
2018-02-18 22:58:00 -0300

    ESLint

--- src/screens/MyMatchesScreen.js
+++ src/screens/MyMatchesScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { StyleSheet, Platform, Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
 import Lang from 'lang'
 import Colors from 'constants/Colors';
