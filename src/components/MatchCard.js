91e0658aef01ba5ae814f87ec3f637a1a51e039a
Nahuel Sotelo <nsotelo@cristalmedia.net>
2018-05-20 15:22:00 -0300

    #23 The map needs to be shown inside a "flex:1" parent

--- src/components/MatchCard.js
+++ src/components/MatchCard.js
@@ -1,62 +1,77 @@
-import React, { Component } from 'react'
-import { ActivityIndicator, StyleSheet, View } from 'react-native'
-import { Card } from 'react-native-elements'
-import { MapView } from 'expo'
+import React, { Component } from "react";
+import { ActivityIndicator, StyleSheet, View, Text } from "react-native";
+import { Card } from "react-native-elements";
+import { MapView } from "expo";
 
 export default class MatchCard extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   state = {
     loading: true,
     marker: false,
     region: {}
-  }
+  };
 
   componentDidMount() {
-    let marker = false
-    const match = this.props.match
-    
+    let marker = false;
+    const match = this.props.match;
+
     let region = {
       latitude: match.location.lat,
       latitudeDelta: 0.0922,
       longitude: match.location.lng,
       longitudeDelta: 0.0421
-    }
-    
+    };
+
     marker = {
       coordinate: {
         latitude: region.latitude,
-        longitude: region.longitude,
+        longitude: region.longitude
       }
-    }
+    };
 
     this.setState({
       loading: false,
       region,
-      marker,
-    })
+      marker
+    });
   }
 
   render() {
-    const theMatch = this.props.match 
+    const theMatch = this.props.match;
     if (this.state.loading) {
       return (
         <View style={styles.container}>
           <ActivityIndicator size="large" />
         </View>
-      )
+      );
     } else {
-      return(
-        <Card title={ theMatch.name }>
-          <MapView style={ styles.map }
-            region={this.state.region} 
-            onRegionChange={ (region) => this.setState({ region }) } 
+      let marker = null;
+      if (this.state.marker) {
+        marker = (
+          <MapView.Marker
+            {...this.state.marker}
+            draggable
+            onDragEnd={event =>
+              this.parseMarkerPosition(event.nativeEvent.coordinate)
+            }
           />
-        </Card>
-      ) 
+        );
+      }
+      return (
+        <View style={styles.container}>
+          <MapView
+            style={styles.map}
+            region={this.state.region}
+            onRegionChange={region => this.setState({ region })}
+          >
+            {marker}
+          </MapView>
+          <Card title={theMatch.name} containerStyle={styles.container} />
+        </View>
+      );
     }
   }
 }
@@ -67,6 +82,5 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1
-  },
-})
-
+  }
+});
